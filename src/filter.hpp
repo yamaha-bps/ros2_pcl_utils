2a3caaab9f10e84cd8bafc24d230273e3f220d7b
Petter Nilsson <pettni@umich.edu>
2021-02-26 15:40:26 -0500

    Fix concurrency issue and optimize (close #2)

--- src/filter.hpp
+++ src/filter.hpp
@@ -15,6 +15,37 @@ namespace bps
 {
 
 /**
+ * @brief Camera projection with OpenCV model
+ *
+ * https://docs.opencv.org/4.2.0/d9/d0c/group__calib3d.html
+ *
+ * @tparam T scalar data type
+ * @param pt_CAM 3D point in camera frame
+ * @param calib camera calibration
+ * @return Eigen::Matrix<T, 2, 1> pixel coordinates
+ */
+template<typename T>
+Eigen::Matrix<T, 2, 1> cameraProject(
+  const Eigen::Matrix<T, 3, 1> & pt_CAM,
+  const bps_msgs::msg::MonoCalibration & calib)
+{
+  const T xp = pt_CAM.x() / pt_CAM.z();
+  const T yp = pt_CAM.y() / pt_CAM.z();
+
+  const T r2 = xp * xp + yp * yp;
+  const T r4 = r2 * r2;
+  const T ratio = 1. + calib.k1 * r2 + calib.k2 * r4 + calib.k3 * (r2 * r4);
+  const T xpp = xp * ratio + 2 * calib.p1 * xp * yp + calib.p2 * (r2 + 2 * xp * xp);
+  const T ypp = yp * ratio + calib.p1 * (r2 + 2 * yp * yp) + 2 * calib.p2 * xp * yp;
+
+  return Eigen::Matrix<T, 2, 1>(
+    calib.fx * xpp + calib.cx,
+    calib.fy * ypp + calib.cy
+  );
+}
+
+
+/**
  * @brief Filter a pointcloud with respect to an image
  *
  * @param[in] img a segmentation image of type mono8
